ed1f50ebe2d0418fe5b05c4853ca3e02ec0508c3
anubhav maurya <channelsub47@gmail.com>
2026-01-09 12:49:37 +0530

    authentication

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
 
+const userRoutes = require('./routes/userroutes');
+
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
   res.send('Referral Management System API');
 });
